ff8a92303c4c30fd8331e35d7247d5eccf20959e
matmat92 <promathieuthiry@gmail.com>
2019-09-29 23:43:06 +0200

    deploy heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const path = require("path")
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -30,6 +31,14 @@ app.use('/users', usersRouter);
 app.use('/auth', authRouter);
 
 // Serve static assests if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
