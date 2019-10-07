254ba93eaeaf835f72fe7c3dd709885405647b41
matmat92 <promathieuthiry@gmail.com>
2019-10-07 20:00:57 +0200

    header

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.use(helmet())
 // app.use(helmet.featurePolicy({features: { notifications: ["'none'"]}}))
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
